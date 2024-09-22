45e3655982829a5300b83802b206344cd2cffde4
Sam Yates <halfflat@gmail.com>
2024-09-22 22:17:19 +0800

    wip

---
+++ rw_domain.py
@@ -0,0 +1,245 @@
+import enum
+import math
+import numbers
+import numpy as np
+
+class Occupancy(enum.IntEnum):
+    EMPTY = 0
+    ODD = 1
+    EVEN = 2
+    FULL = 3
+
+    def flip(self):
+        match self:
+            case Occupancy.EVEN: return Occupancy.ODD
+            case Occupancy.ODD:  return Occupancy.EVEN
+            case _: return self
+
+    def parity(t):
+        return Occupancy.ODD if t%2 else Occupancy.EVEN
+
+class Domain:
+    # Given a PeriodicNet _net_, a tuple _shape_ describing the extent, and a
+    # _maximum occupancy_ per vertex, create the domain over which the walk is
+    # perfomed.
+    #
+    # The extent tuple must have the same dimension as the periodic net.
+    #
+    # The domain is represented by an array with extents (I+2, J+2, K+2, V, M)
+    # in the 3-dimensional case, given a shape tuple (I, J, K), a net with V
+    # vertices, and a maximum occupancy M. The spatial boundary (in the I, J, K
+    # dimensions) is marked with special values indicating that the vertices
+    # are in fact boundary values.
+    #
+    # Domain access is by coordinates (i, j, k, v) (for 3-dimensional case) or
+    # (i, j, v) (2-dimensional case) where (i, j, k) are the cell-coordinates
+    # and v is the vertex-index. These coordinates are offset by the boundary
+    # padding, that is, i, j, and k are incremented before accessing the
+    # internal domain array.
+    #
+    # The array holds 32-bit values representing the (packed) information
+    # describing each occupant: the neuron id (24 bits); occupancy class
+    # (2 bits); and previous edge index (6 bits).
+
+    BORDER = np.invert(np.uint32(0))
+    NOEDGE = np.uint8(63)
+    OCCUPANT_DTYPE = np.dtype([('nid', np.uint32), ('occupancy', np.uint8), ('prev', np.uint8)])
+
+    def __init__(self, net, shape, max_occupancy, priority):
+        """
+        Create and initialize a domain for lattice random walk
+
+        Keyword arguments
+        net              -- a PeriodicNet object describing the lattice
+        shape            -- tuple (I, J) or (I, J, K) describing extent of
+                            domain in lattice cells
+        max_occupancy    -- maximum number of residents per lattice site
+        priority         -- function p(nid, vtx_hash) that returns the
+                            numerical priority of neuron nid coming from the
+                            vertex represented by the integer vtx_hash
+        """
+
+        self._mo = max_occupancy;
+        self._net = net
+        slef._priority_fn = priority
+
+        if self._net.dimension != len(shape):
+            raise Exception("shape is wrong dimension")
+
+        if self._net.degree>63:
+            raise Exception("only supporting up to degree 63 with packed representation")
+
+        # make domain but fill boundary with boundary marker
+        inner = shape + (self._net.n_vertices, self._mo)
+        pad = [(1,1)]*len(inner)
+        pad[-2:] = [(0,0), (0,0)]
+
+        self._pad_offset = np.ones((len(inner),), dtype=np.int32)
+        self._pad_offset[-2:] = [0, 0]
+
+        self._domain = np.pad(np.zeros(inner, dtype=np.uint32), pad, mode='constant', constant_values=Domain.BORDER)
+
+        # pre-calculate offsets, shifts for computing vertex index hashes
+        self._vext = self._domain.shape[:-1]
+        self._vdim = len(self._vext)
+        self._hw = np.asarray([0]+[int(k-1).bit_length() for k in self._vext])
+        self._ho = self._hw.cumsum()
+
+    # Return extent, excluding the padded boundaries and occupancy axis.
+    extent = property(lambda self: (self._domain.shape - self._pad_offset*2)[:-1])
+
+    # Return number of occupancy slots per vertex
+    max_occupancy = property(lambda self: self._mo)
+
+    def priority(self, nid, vertex_or_hash):
+        if isinstance(vertex_or_hash, numbers.Integral):
+            return self._priority_fn(nid, vertex_or_hash)
+        else:
+            return self._priority_fn(nid, self.vertex_hash(vertex_or_hash))
+
+    def unpack(packed):
+        u = np.recarray(packed.shape, dtype=Domain.OCCUPANT_DTYPE)
+        u.nid = np.right_shift(packed, 8)
+        u.occupancy = np.right_shift(np.bitwise_and(packed, 255), 6)
+        u.prev = np.bitwise_and(packed, 63)
+        return u
+
+    def pack(unpacked):
+        return np.array(np.left_shift(unpacked.nid, 8) + np.left_shift(unpacked.occupancy, 6) + unpacked.prev, dtype=np.uint32)
+
+    # Convert vertex index to and from packed bit representation
+    def vertex_hash(self, vertex):
+        return (np.asarray(vertex) << self._ho[:-1]).sum()
+
+    def vertex_unhash(self, h):
+        return (h >> self._ho[:-1]) & ((1 << self._hw[1:]) - 1)
+
+    vertex_hash_bits = property(lambda self: self._ho[-1])
+
+    # Return occupant in given slot in the domain at vertex, or all occupants at vertex if slot is None.
+    def get(self, vertex, slot = None):
+        index = np.asarray(vertex) + self._pad_offset[:-1]
+        if slot is None:
+            return self._domain[tuple(index)+(slice(None),)]
+        else:
+            return self._domain[tuple(index)+(slot,)]
+
+    # Update occupant at slot in the domain at vertex with packed value, or all occupants with array of packed values if slot is None.
+    def put(self, vertex, occupant, slot = None):
+        index = np.asarray(vertex) + self._pad_offset[:-1]
+        if slot is None:
+            self._domain[tuple(index)+(slice(None),)] = occupant
+        else:
+            self._domain[tuple(index)+(slot,)] = occupant
+
+    def traverse(self, vertex, edge_index):
+        return self._net.traverse(vertex, edge_index)
+
+    def traverse_r(self, vertex, edge_index):
+        return self._net.traverse_r(vertex, edge_index)
+
+    def position(self, vertex):
+        return self._net.position(vertex)
+
+    def is_border(self, vertex):
+        index = np.asarray(vertex) + self._pad_offset[:-1]
+        return self.get(vertex, 0)==self.BORDER
+
+    def has_nid(self, vertex, nid, parity):
+        """
+        Test if site at vertex holds nid unprovisionally.
+
+        Return True if any slot has matching nid with occupancy FULL or opposite of _parity_.
+        """
+        up = Domain.unpack(self.get(vertex))
+        return ((up.nid == nid) & ((up.occupancy == Occupancy.FULL) | (up.occupancy == parity.flip()))).any()
+
+    def has_other_nid(self, vertex, nid, parity):
+        """
+        Test if site at vertex holds a different nid unprovisionally.
+
+        Return True if any slot has a different nid with occupancy FULL or opposite of _parity_.
+        """
+        up = Domain.unpack(self.get(vertex))
+        return ((up.nid != nid) & ((up.occupancy == Occupancy.FULL) | (up.occupancy == parity.flip()))).any()
+
+    def is_full(self, vertex, parity):
+        """
+        Test if site at vertex may accomodate another neuron.
+
+        Returns True if every slot has occupancy FULL or opposite of _parity_.
+        """
+        up = Domain.unpack(self.get(vertex))
+        return not ((up.occupancy == Occupancy.FULL) | (up.occupancy == parity.flip())).any()
+
+    def neighbours(self, vertex):
+        """Non-border neighbours [(u,e)] of vertex with u = traverse(vertex, e)."""
+        return [(u, e) for e in self._net.edge_indices_from(vertex[-1]) for u in [self.traverse(vertex, e)] if not self._is_border(u)]
+
+    def open_neighbours_excluding(self, vertex, nid):
+        """Non-border neighbours [(u,e)] of vertex which are not full and do
+           not hold nid with u = traverse(vertex, e)."""
+        return [(u, e) for e in self._net.edge_indices_from(vertex[-1]) for u in [self.traverse(vertex, e)] if not self.is_border(u)
+                and not self.is_full(v) and not self.has_nid(nid)]
+
+    def occupy(self, vertex, nid, parity, prev):
+        """
+        Provisionally place (nid, parity, prev) in a slot at vertex.
+
+        A placement will succeed if there is an EMPTY slot or a provisional
+        slot, viz. a slot with the same occupancy _parity_, with lower
+        priority. A vertex can host at most one instance of a neuron nid so
+        if nid is already present, placement can only succeed if that instance
+        is provisional and of lower priority.
+
+        Returns None on failure, or else a pair (evicted, slot-index) where
+        evicted is the id of a neuron that was removed to make room for this
+        neuron (or zero, if the slot was empty) and slot-index indicates which
+        occupancy slot was occupied.
+        """
+
+        up = Domain.unpack(self._domain.get(vertex))
+        slot = None   # or (slot index, slot priority)
+
+        for j in range(self._mo):
+            if up[j].nid == nid:
+                if up[j].occupancy != parity:
+                    return None
+                else:
+                    slot = (j, self.priority(up[j].nid, self.traverse_r(vertex, up[j].prev)))
+                    break
+
+            if up[j].occupancy == parity:
+                p = self.priority(up[j].nid, self._domain.traverse_r(vertex, up[j].prev))
+                if slot is None or p <= slot[1]:
+                    slot = (j, p)
+            elif up[j].occupancy = Occupancy.EMPTY:
+                slot = (j, 0)
+
+        if slot is not None:
+            (s, p) = slot
+            evict = up[s].nid
+            q = self.priority(nid, self._domain.traverse_r(vertex, prev))
+
+            if (q, nid) > (p, evict):
+                up[s] = (nid, parity, prev)
+                self.put(vertex, Domain.pack(up[s]), s)
+                return (evict, s)
+
+        return None
+
+    def resolve_provisional(self, vertex, slot, nid, prev):
+        """
+        Confirm neuron nid from vertex determined by prev has not been evicted from slot.
+
+        If nid and prev match, set slot occupamny to FULL and return True.
+        If nid or prev do not mach, return False.
+        """
+
+        u = Domain.unpack(self.get(vertex, slot))
+        if u.nid != nid or u.prev != prev:
+            return False
+        else:
+            u.occupancy = Occupancy.FULL
+            self.put(vertex, slot, Domain.pack(u))
+            return True
--- rw_serial.py
+++ rw_serial.py
@@ -21,19 +21,29 @@
 # directly; we wrap the NumPy Philox bit generator in a class that munges
 # the counter value in order to emulate this.
 
-import numpy as np
-import enum
 import sys
-from periodic_net import Domain
+from collections import namedtuple
+import numpy as np
+import periodic_net as pn
+from rw_domain import Domain, Occupancy
+
 
 class Philox:
+    """
+    Wraps numpy.random.Philox bit generator.
+    Provides method to set counter value explicitly.
+    """
+
     # 'types' of function used in key construction
     GENERIC = 0
     INITIAL_SITE = 1
     PROPAGATION = 2
     PRIORITY = 3
 
-    def __init__(self, seed, fn = self.GENERIC)
+    def __init__(self, seed, fn = GENERIC):
+        """
+        Create wrapped Philox generator with key derived from 64-bit seed and integer _fn_.
+        """
         if seed < 0 or seed >= (1<<64):
             raise ValueError("seed must be representable as a 64-bit unsigned integer")
         key = (fn<<64)+seed
@@ -50,6 +60,7 @@ class Philox:
         np.copyto(self._state0.get('state').get('counter'), ~np.zeros((4,), dtype=np.uint64))
         self._bg.state = self._state0
 
+    @property
     def bit_generator(self):
         return self._bg
 
@@ -57,7 +68,7 @@ class Philox:
         if counter==0:
             self.reset()
         else:
-            np.copyto(self._state0.get('state').get('counter'), np.frombuffer((counter-1).to_bytes(32, sys.byteorder), dtype=np.uint64))
+            np.copyto(self._state0.get('state').get('counter'), np.frombuffer((int(counter)-1).to_bytes(32, sys.byteorder), dtype=np.uint64))
             self._bg.state = self._state0
 
     def __call__(self, counter = None):
@@ -75,91 +86,187 @@ class Philox:
 def random_integer(bitgen, u):
     return np.random.Generator(bitgen).integers(u)
 
-def mk_priority_map(domain, seed):
-    if domain.vertex_hash_bits>64:
-        raise ValueError("domain extent is unexpectedly large")
-
+def mk_priority_fn(seed):
+    """Construct a priority funciton f(nid, vtx_hash) for Domain."""
     hasher = Philox(seed, Philox.PRIORITY)
-    def f(nid, vertex_or_hash):
-        if type(vertex_or_hash) is int:
-            return hasher((nid<<64)+vertex_or_hash)
-        else:
-            return hasher((nid<<64)+domain.vertex_hash(vertex_or_hash))
+    def f(nid, vtx_hash):
+        return hasher((nid<<64)+vtx_hash)
     return f
 
-def mk_initial_map(domain, seed):
+def mk_propagation_generator(seed):
+    """Construct a funciton g(nid, vtx_hash, t) returning a BitGenerator."""
+    p = Philox(seed, Philox.PROPAGATION)
+    def g(nid, vtx_hash, t):
+        p.set_counter((t<<96)+(nid<<72)+(vtx_hash<<8))
+        return p.bit_generator
+    return g
+
+def mk_initial_random_vertex(domain, seed):
     hasher = Philox(seed, Philox.INITIAL_SITE)
     def f(nid, attempt):
-        hasher.set_counter((nid<<64)+(attempt<,32))
-        return random_integer(hasher, domain.extent())
+        hasher.set_counter((nid<<64)+(attempt<<32))
+        return random_integer(hasher.bit_generator, domain.extent())
     return f
 
+
 class State:
-    class Tip(namedtuple('Tip', ['vertex', 'from_edge']):
-            stymied(self): return self.from_edge<0
-            stymy(self): self.from_edge = -1
+    MAX_INITIAL_RETRY = 100
 
-    def __init__(domain, n_neurons, seed = 0)
-        self._domain = domain
-        self._cbrng = Philox(seed)
+    # vertex is index tuple for domain; prev is the edge index from previous site to vertex;
+    # slot is the slot in the site occupancy table at vertex where we expect to find this
+    # tip unless it has been evicted.
+    #
+    # TODO: use a numpy compound datatype to save space?
+
+    class Tip(namedtuple('Tip', ['vertex', 'prev', 'slot'])):
+        def is_stymied(self):
+            return self.slot is None
+
+        def stymied(vertex):
+            return Tip(vertex=vertex, prev=Domain.NOEDGE, slot=None)
+
+    def __init__(self, n_neurons, net, shape, max_occupancy, seed = 0):
+        """
+        Construct random walk state.
+
+        Keyword arguments:
+        n              -- number of neurons
+        net            -- PeriodicNet object describing lattice
+        shape          -- number (I, J) or (I, J, K) of lattice cells in domain
+        max_occupancy  -- maximum number of neurons resident per site in domain
+        """
+
+        self._net = net
+        self._domain = Domain(net, shape, max_occupancy, mk_priority_fn(seed))
         self._n = n_neurons
-        self.priority = mk_priority_map(domain, seed)
+        self._t = 0
+        self._multiple_occupancy = max_occupancy > 1
+        self._propagation_generator = mk_propagation_generator(seed)
 
-        # TODO: special case initial state generation when vertices in domain
-        # are of a similar order as the number of neurons (use a reservoir method).
-
-# notes for later:
-# tip datastructure: list (indexed by nid -1) of:  list or np array of tip type
-# tip type: (vertex, former edge index | stymied)
-#
-# to generalize to parallel initialization, use priority scheme to resolve
-# collisions in initialization; maintain list of evicted or frustrated neuron ids;
-# retry until all resolved (or give up if iteration count starts getting 'large').
-
-        self._tips = [] * self._n
-
+        self._tips = [[]] * self._n
+        random_vertex = mk_initial_random_vertex(self._domain, seed)
         retry = []
-        for i in range(0, n_neurons):
+        attempt = 0
+        for i in range(0, self._n):
             nid = i + 1
-            v = 
+            v = random_vertex(nid, attempt)
+            match self.occupy(v, nid, Domain.Occupancy.EVEN, Domain.NOEDGE):
+                case (0, slot):
+                    self._tips[nid-1] = [State.Tip(vertex=v, prev=Domain.NOEDGE, slot=slot)]
+                case (evicted, slot):
+                    self._tips[nid-1] = [State.Tip(vertex=v, prev=Domain.NOEDGE, slot=slot)]
+                    self._tips[evicted-1] = []
+                    retry.append(evicted)
+                case None:
+                    retry.append(nid)
 
-
-
-
-
-    def occupy(self, vertex, nid, parity, edge):
-        # Can provisionally add neuron nid to vertex if there is an EMPTY slot, or if
-        # the least priority of a slot with same parity is less than that of (vertex, nid).
-        # Return None on failure, True if occupied an empty slot, or the nid of evicted
-        # neuron if the slot was occupied.
-
-        v_hash = self.domain.vertex_hash(vertex)
-        mo = self.domain.max_occupancy
-        up = Domain.unpack(self.domain.get(vertex))
-
-        min_priority = 2**64 - 1
-        min_slot = -1
-        for s in range(mo):
-            if up[s].occupancy == Domain.Occupancy.EMPTY:
-                up[s] = (nid, parity, edge)
-                domain.put(vertex, s, Domain.pack(up[s]))
-                return True
-            elif up[s].occupancy == parity:
-                p = self.priority(up[s].nid, v_hash)
-                if p <= min_priority:
-                    min_priority = p
-                    min_slot = s
-
-        if min_slot<0:
-            return None
-        else:
-            p = self.priority(nid, v_hash)
-            evicted = up[min_slot].nid
-            if (p, nid) > (min_priority, evicted):
-                up[min_slot] = (nid, parity, edge)
-                domain.put(vertex, min_slot, Domain.pack(up[min_slot]))
-                return evicted
+        while retry:
+            nid = retry[-1]
+            del retry[-1]
+            for attempt in range(1,State.MAX_INITIAL_RETRY+1):
+                v = random_vertex(nid, attempt)
+                match self.occupy(v, nid, Domain.Occupancy.EVEN, Domain.NOEDGE):
+                    case (0, slot):
+                        self._tips[nid-1] = [State.Tip(vertex=v, prev=Domain.NOEDGE, slot=slot)]
+                        break
+                    case (evicted, slot):
+                        self._tips[nid-1] = [State.Tip(vertex=v, prev=Domain.NOEDGE)]
+                        self._tips[evicted-1] = []
+                        retry.append(evicted)
+                        break
+                    case None:
+                        continue
             else:
-                return None
+                raise Exception("unable to place neuron {}".format(nid))
 
+        # Keep a copy of initial neuron sites so that neuron morphology can be reconstructed.
 
+        self.initial = np.array((nid, len(_domain.extent)), dtype=np.uint32)
+        for i, tip in enumerate(self._tips):
+            self.initial[i,:] = np.asarray(tip.vertex, dtype=np.uint32)
+
+    def step:
+        self._t += 1
+        parity = Occupancy.parity(self._t)
+
+        n_exhausted = 0
+        for i in range(0, self._n):
+            nid = i + 1
+            ts = self._tips[i]
+
+            # Work backwards through list of tips: new tips from branching are appended;
+            # tips that expire are replaced by the tip at the end, and the end is then removed.
+            for j in range(len(ts)-1, -1, -1):
+                tip = ts[j]
+
+                # Check if tip was evicted, fix occupancy state.
+                if not tip.is_stymied():
+                    if not self._domain.resolve_provisional(tip.vertex, tip.slot, nid, tip.prev):
+                        assert tip.prev != Domain.NOEDGE
+                        ts[j] = Tip.stymied(self._domain.traverse_r(tip.vertex, tip.prev))
+
+                if self._multiple_occupancy:
+                    self.step_mo(parity, nid, ts, j)
+                else:
+                    self.step_so(parity, nid, ts, j)
+
+            if not self._tips[i]: n_exhausted += 1
+
+        return n_exhausted != self._n
+
+    def step_mo(parity, nid, ts, j):
+        # mo > 1 case:
+        # 1. Branch if tip is not stymied and site has shared occupancy.
+        # 2. Select and propagte to new sites from neighbours which are (provisionally)
+        #    not full and which are not already occupied by same nid.
+
+        tip = ts[j]
+        branch = not tip.stymied() and self._domain.has_other_nid(tip.vertex, nid, parity)
+        neighbours = self._domain.open_neighbours_excluding(tip.vertex, nid)
+
+        if not neighbours:
+            # just remove this tip
+            ts[j] = ts[-1]
+            del ts[-1]
+            return
+
+        bg = None
+        if len(neighbours)==1:
+            k = 0
+        else:
+            bg = self._propagation_generator(nid, self._domain.vertex_hash(vertex), t)
+            k = random_integer(bg, len(neighbours))
+
+        (u, e) = neighbours[k]
+        match self._domain.occupy(u, nid, parity, e):
+            case (_, s): ts[j] = State.Tip(vertex=u, prev=e, slot=s)
+            case None:   ts[j] = State.Tip.stymied(tip.vertex)
+
+        if not branch or len(neighbours)<2:
+            return
+
+        neighbours[k] = neighbours[-1]
+        del neighbours[-1]
+        if len(neighbours)==1:
+            k = 0
+        else:
+            k = random_integer(bg, len(neighbours))
+
+        (u, e) = neighbours[k]
+        match self._domain.occupy(u, nid, parity, e):
+            case (_, s): ts.append(State.Tip(vertex=u, prev=e, slot=s))
+            case None:   ts.append(State.Tip.stymied(tip.vertex))
+
+    def step_so(parity, nid, ts, j):
+        # mo == 1 case:
+        # 1. Select new site from neighbours which are not already occupied
+        #    by same nid.
+        # 2. If new site is full, branch and:
+        #    3. Select and propagate to new sites from neighbours
+        #       which are (provisionally) not full and not already occupied by
+        #       same nid.
+        #    Otherwise:
+        #    3. Propagate to new site.
+
+        tip = ts[j]
+        ...
